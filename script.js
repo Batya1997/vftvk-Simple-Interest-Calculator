931fbb3f8a6ea0eb0e631fbdcc7ee8f2fc8bdf7f
Batya1997 <bakhtiyar.mammadov1997@gmail.com>
2022-05-17 14:31:14 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
+
+
 function compute()
 {
-    p = document.getElementById("principal").value;
+principal = document.getElementById("principal").value;
+rate = document.getElementById("rate").value;
+years = document.getElementById("years").value;
+interest = principal * years * rate /100;
+year = new Date().getFullYear()+parseInt(years);
+
+if(principal == undefined || principal <= 0){
+    alert("Enter a Positive Number");
+    document.getElementById("principal").focus();
     
+    return
+}else{
+    // Display the text with the calculated values
+    var inr = "If you deposit <mark>" + principal +",</mark><br>";
+    var ra = "at an interest rate of <mark>"+ rate +" %,</mark><br>";
+    var am="You will receive an amount of <mark>" +interest + ",</mark><br>"
+    var ye ="in the year <mark>"+year +"</mark>";
+
+    document.getElementById("result").innerHTML=inr+ra+am+ye;
+
 }
-        
+
+}
+//To update the Rate Value
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+rate.onchange = function()
+
+{  rate_val.innerHTML = this.value;     }
+
+
